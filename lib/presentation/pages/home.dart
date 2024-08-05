dae1d12154861d8853c5e5af140e846684f1a777
Nguyen-Le-Tuan <tuan.nguyenle2008@gmail.com>
2024-08-05 23:19:47 +0700

    done

--- lib/presentation/pages/home.dart
+++ lib/presentation/pages/home.dart
@@ -1,34 +1,95 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_svg/flutter_svg.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Padding(
-          padding: const EdgeInsets.only(top: 10.0), // Adjust the top padding as needed
-          child: Text(
-            "Let's learn 💪",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 35,
-              fontWeight: FontWeight.bold,
+      appBar: appBar(),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          searchTextField(),
+          const SizedBox(height: 40,),
+          const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left:20),
+                child: Text(
+                  'Category',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Container searchTextField() {
+    return Container(
+          margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Color(0xff1D1617).withOpacity(0.11),
+                blurRadius: 40,
+                spreadRadius:0.0
+              )
+            ]
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              contentPadding: EdgeInsets.all(15),
+              hintText: "Search Vocabulary260724",
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(12),
+                child: SvgPicture.asset('assets/icon/Search.svg'),
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide.none
+              )
             ),
           ),
-        ),
-        backgroundColor: Colors.orangeAccent,
-        elevation: 0.0,
-        centerTitle: true,
-        leading: Container(
-          margin: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: Colors.black,
-            borderRadius: BorderRadius.circular(10)
-          ),
+        );
+  }
+
+  AppBar appBar(){
+    return AppBar(
+      title: Text(
+        "    Let's learn 💪",
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 35,
+          fontWeight: FontWeight.bold,
         ),
       ),
+      backgroundColor: Colors.orangeAccent,
+      elevation: 0.0,
+      centerTitle: true,
+      actions: [
+        Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          child: SvgPicture.asset('assets/icon/bell.svg'),
+          height: 30,
+          width: 30,
+          decoration: BoxDecoration(
+            // color: Color(0xffF7F8F8),
+              borderRadius: BorderRadius.circular(10)
+          ),
+        ),
+      ],
     );
   }
 }
